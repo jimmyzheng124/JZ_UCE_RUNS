2fa6aecd62fa9509eb516aa1ca3ee51e8dda25dd
jimmyzheng124 <jimmyzheng124@ucla.edu>
2015-12-10 09:00:35 -0800

    Adding Exabayes 95p tree with support values

--- trees/new-assembly/viewTree.py
+++ trees/new-assembly/viewTree.py
@@ -1,4 +1,16 @@
-import dendropy; import os, os.path; import re;
+import dendropy; import os, os.path; import re; import sys; import glob;
+
+print "Choose tree from below to view: "
+print '\n'.join([os.path.basename(x) for x in glob.glob("./*malawi*")])
+phyinput = raw_input("Enter tree: ")
 from dendropy import TaxonSet, Tree, TreeList, DnaCharacterMatrix, DataSet;
-tree = dendropy.Tree.get_from_path("RAxML_bestTree.best", schema="newick")
-tree.print_plot();
+for retry in range(0,9):
+	try:
+		tree = dendropy.Tree.get_from_path(phyinput, schema="newick")
+		tree.print_plot()
+	except:
+		print "Invalid tree. Try inputting your selection again:"
+		phyinput = raw_input()
+		pass
+	if retry == 8:
+		print "Let's not waste time here. Good bye."
